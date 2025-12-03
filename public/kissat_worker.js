09cbf820ffb879bf54e2061291eb55484bf8dd79
PoorShack <example@example.com>
2025-12-03 12:29:46 +0800

    Add kissat SAT solver

---
+++ public/kissat_worker.js
@@ -0,0 +1,31 @@
+importScripts("kissat-emscripten.js", "kissat.js");
+
+onmessage = (ev) => {
+  const {vars, clauses} = ev.data;
+
+    console.log("Worker received message", ev.data);
+    self.solver = new Kissat();
+    self.solver.initSat();
+    // for (const lit of vars) {
+    //   self.solver.add(lit);
+    // }
+    // self.solver.add(0); // terminate vars
+
+    for (const clause of clauses) {
+      for (const lit of clause) {
+        self.solver.add(lit);
+      }
+      // self.solver.addClause(clause);
+      self.solver.add(0); // terminate clause
+    }
+
+    delete ev.data.clauses;
+    console.log("Clauses added, starting to solve...");
+
+  
+    const sat = self.solver.solve();
+    const model = vars ? self.solver.model(vars) : null;
+    self.solver.release();
+    postMessage({ sat, model });
+    console.log("Worker sent message", { sat, model });
+};
